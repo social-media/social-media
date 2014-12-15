768b8b34355df7c999c57e8905a73b929760d22d
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-12-15 17:06:27 +0100

    Exception added

---
+++ src/Exception.php
@@ -0,0 +1,11 @@
+<?php
+namespace JeroenDesloovere\SocialMedia;
+
+/**
+ * Social Media Exception class
+ *
+ * @author Jeroen Desloovere <jeroen@siesqo.be>
+ */
+class Exception extends \Exception
+{
+}
