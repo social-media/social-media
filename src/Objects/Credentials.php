272eaa4445b71262dcb7e0148e701db038138422
Jeroen Desloovere <jeroendesloovere@gmail.com>
2014-12-16 10:09:37 +0100

    Credentials class added

---
+++ src/Objects/Credentials.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SocialMedia\Objects;
+
+/*
+ * This file is part of the Social Pushing from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Credentials to use in a service
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class Credentials
+{
+
+}
