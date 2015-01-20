b10f573d314127a3f30d9756d783ff27e115d2c9
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-01-20 16:05:34 +0100

    SocialMediaTest updated

--- tests/SocialMediaTest.php
+++ tests/SocialMediaTest.php
@@ -13,10 +13,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
  */
 
 use SocialMedia\Core\SocialMedia;
-use SocialMedia\Core\Objects\Exception;
-use SocialMedia\Core\Objects\Post;
-use SocialMedia\Core\Objects\Service;
-use SocialMedia\Core\Objects\Credentials;
+use SocialMedia\Core\Objects\Timeline;
 
 /**
  * Social Media Class which contains everything to (un)publish
@@ -27,18 +24,6 @@ use SocialMedia\Core\Objects\Credentials;
 class SocialMediaTest extends \PHPUnit_Framework_TestCase
 {
     protected static $api;
-
-    /**
-     * Set up extra variables
-     */
-    protected function setUp()
-    {
-        $this->service = new Service(
-            array(),
-            new Credentials()
-        );
-        $this->post = new Post();
-    }
 
     /**
      * Set up before class
@@ -53,40 +38,16 @@ class SocialMediaTest extends \PHPUnit_Framework_TestCase
     /**
      * Tear down after class
      */
-    public function tearDown()
-    {
-        $this->service = $this->post = null;
-    }
-
-    /**
-     * Tear down after class
-     */
     public static function tearDownAfterClass()
     {
         self::$api = null;
     }
 
     /**
-     * @expectedException Exception
-     */
-    public function testException()
-    {
-        throw new Exception('error');
-    }
-
-    /**
      * Test publish function
      */
-    public function testPublish()
+    public function testTimeline()
     {
-        self::$api->getTimeline()->publish($this->service, $this->post);
-    }
-
-    /**
-     * Test unpublish function
-     */
-    public function testUnpublish()
-    {
-        self::$api->getTimeline()->unpublish($this->service, $this->post);
+        $this->assertEquals(new Timeline(), self::$api->getTimeline());
     }
 }
