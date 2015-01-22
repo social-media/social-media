85e039be6277d853d75006411f68704e1f544cb0
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-01-22 11:55:07 +0100

    ServiceTest added

---
+++ tests/Objects/ServiceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SocialMedia\Core\tests;
+
+// required to load
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+/*
+ * This file is part of the Social Pushing from Jeroen Desloovere.
+ *
+ * For the full copyright and license information, please view the license
+ * file that was distributed with this source code.
+ */
+
+use SocialMedia\Core\Objects\Service;
+
+/**
+ * Social Media Service Test.
+ *
+ * @author Jeroen Desloovere <info@jeroendesloovere.be>
+ */
+class ServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Set up extra variables
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->service = new Service();
+        $this->name = 'SocialMediaService';
+    }
+
+    /**
+     * Tear down after class
+     */
+    public function tearDown()
+    {
+        $this->service = null;
+        parent::tearDown();
+    }
+
+    /**
+     * Test a parameter
+     */
+    public function testName()
+    {
+        $this->service->setName($this->name);
+        $this->assertEquals($this->name, $this->service->getName());
+    }
+}
